9efc58081471fa2b3635ad97844089832d49e81e
dadombom <117142834+dadombom@users.noreply.github.com>
2023-01-07 16:53:09 -0800

    Made a scene to work in and a script for resource management

---
+++ 4 Day Work Week/Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ResourceManager : MonoBehaviour
+{
+    int coalCount;
+    int maxCoalCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void AddCoal(int amount)
+    {
+        coalCount += amount;
+    }
+
+    void RemoveCoal(int amount)
+    {
+
+    }
+}
