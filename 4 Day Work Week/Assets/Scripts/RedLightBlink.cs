e7048875c9cb1ecbc6793e0fd73d64fd5017cc0d
ShaeSouterWillett <shaesouterwillett@gmail.com>
2023-01-14 21:59:52 -0800

    radio blink

---
+++ 4 Day Work Week/Assets/Scripts/RedLightBlink.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedLightBlink : MonoBehaviour
+{
+    Light myLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myLight.intensity = Mathf.PingPong(Time.time/4, 1);
+    }
+}
