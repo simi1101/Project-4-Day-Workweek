22f0b9967b89faa437d243cf5fb5d04db465615d
LittleWoodlands <gabrielsamaro@gmail.com>
2023-01-15 20:43:30 -0800

    no message

--- 4 Day Work Week/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ 4 Day Work Week/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,6 +14,7 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID FOOTSTEPS = 2385628198U;
+        static const AkUniqueID PLAY_EVIROMENT_RANDOM = 2434186889U;
     } // namespace EVENTS
 
     namespace SWITCHES
