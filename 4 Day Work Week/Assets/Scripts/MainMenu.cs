359a34079f71c19ba813d3c50563c7fd53588220
Joey Fici <ajfici1@gmail.com>
2023-01-16 21:12:39 -0500

    Finished main menu prefab, added pause menu prefab and script

--- 4 Day Work Week/Assets/Scripts/MainMenu.cs
+++ 4 Day Work Week/Assets/Scripts/MainMenu.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public GameObject mainMenuUI;
     public GameObject player;
     public GameObject lantern;
-    public GameObject dial;
-    
+    public GameObject gauge;
+
+    public AK.Wwise.Event ExpEvent;
+
     // Start is called before the first frame update
     void Start()
     {
         player.GetComponent<InputManager>().enabled = false;
         mainMenuUI.SetActive(true);
         lantern.SetActive(false);
-        dial.SetActive(false);
+        gauge.SetActive(false);
+
+        // Play title music
+        ExpEvent.Post(gameObject);
     }
 
     // Update is called once per frame
@@ -26,14 +32,22 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
-        player.GetComponent<InputManager>().enabled = true;
+        player.GetComponent<InputManager>().enabled = !false;
         mainMenuUI.SetActive(false);
         lantern.SetActive(true);
-        dial.SetActive(true);
+        gauge.SetActive(true);
+
+        // Stop playing title music
+        ExpEvent.Stop(gameObject);
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void PlayCredits()
+    {
+        //SceneManager.LoadScene(0);
     }
 }
--- 4 Day Work Week/Assets/Scripts/PauseMenu.cs
+++ 4 Day Work Week/Assets/Scripts/PauseMenu.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenuUI;
-    public float pauseButton;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,34 +16,29 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //pauseButton = Input.GetAxis("Cancel");
-
-
 
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            PauseGame();
-        }
-        else
-        {
-            PlayGame();
+            pauseMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            Debug.Log("Pausing");
         }
     }
 
-    public void PauseGame()
+    public void ResumeGame()
     {
-        pauseMenuUI.SetActive(true);
-        Time.timeScale = 0f;
-    }
-    
-    public void PlayGame()
-    {
-        Time.timeScale = 1f;
         pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        Debug.Log("Resuming");
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void PlayCredits()
+    {
+        //SceneManager.LoadScene(0);
     }
 }
