c6cc15bdf96ab8aca5499d80fe4d0cf70a97b1dd
dadombom <117142834+dadombom@users.noreply.github.com>
2023-01-16 23:19:16 -0800

    Credits

--- 4 Day Work Week/Assets/Scripts/Credits.cs
+++ 4 Day Work Week/Assets/Scripts/Credits.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class Credits : MonoBehaviour
 {
+    float timer;
 
     public AK.Wwise.Event creditMusic;
     // Start is called before the first frame update
     void Start()
     {
         creditMusic.Post(gameObject);
+        timer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        if(timer >= 65)
+        {
+            Application.Quit();
+        }
     }
 }
